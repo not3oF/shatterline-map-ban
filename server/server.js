ecf93ba721c37815b5452217a28cf5140dcfd609
not3oF <tuukka.peuralahti@gmail.com>
2023-04-28 23:38:22 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80;
 const {Users} = require('./utils/userStorage');
 
 var app = express();
